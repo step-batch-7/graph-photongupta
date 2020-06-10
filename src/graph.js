1a638e8d142a8e8abcdb3d4f7bdbbd2f8a81934e
Rashmi Gupta <rashmi224141@gmail.com>
2020-06-10 21:00:20 +0530

    Added bfs function

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,38 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const parse = function (pairs) {
+  const data = pairs.slice();
+  const paths = {};
+  data.forEach(([source, target]) => {
+    if (!Object.keys(paths).includes(source)) {
+      paths[source] = [];
+    }
+    paths[source].push(target);
+  });
+  return paths;
+};
+
+const bfs = function (pairs, source, target) {
+  const paths = parse(pairs);
+  const visited = [];
+  const queue = [source];
+  while (queue.length) {
+    const current = queue.pop();
+    visited.push(current);
+    if (current == target) {
+      return true;
+    }
+    paths[current].forEach((e) => {
+      if (!visited.includes(e) && !queue.includes(e)) {
+        queue.push(e);
+      }
+    });
+  }
+  return false;
 };
 
 module.exports = {bfs};
